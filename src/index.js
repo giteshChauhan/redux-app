1e23b9db0bcc3039e5f67309cbd71370b7e4a342
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-11-26 22:27:55 +0530

    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log("Hello World!");
+import * as actions from "./actionTypes";
+import { produce } from "immer";
+import store from "./store";
+
+const unsubscribe = store.subscribe(() => {
+  console.log("Store changed!", store.getState());
+});
+
+store.dispatch({
+  type: actions.BUG_ADDED,
+  payload: {
+    description: "Bug1",
+  },
+});
+
+unsubscribe();
+
+store.dispatch({
+  type: actions.BUG_REMOVED,
+  payload: {
+    id: 1,
+  },
+});
