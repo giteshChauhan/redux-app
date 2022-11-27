27ba4ec7380a268884a6be96b9e2b23ce8153540
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-11-27 19:28:20 +0530

    updated configureStore.js | used redux-toolkit

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,8 +1,6 @@
-import { devToolsEnhancer } from "redux-devtools-extension";
-import { createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import reducer from "./bugs";
 
-export default function configureStore() {
-  const store = createStore(reducer, devToolsEnhancer({ trace: true }));
-  return store;
+export default function () {
+  return configureStore({ reducer });
 }
