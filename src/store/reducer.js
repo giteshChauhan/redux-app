e2b93d2b202837d1f0726026cf67a4a7068b5742
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-11-28 18:05:31 +0530

    updated reducers.js | added userReducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import entitiesReducer from "./entities";
+import userReducer from "./users";
 
 export default combineReducers({
   entities: entitiesReducer,
+  users: userReducer,
 });
