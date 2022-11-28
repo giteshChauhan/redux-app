4f34cd6e96c7c2ba324831c653db57b97e6d9330
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-11-28 18:07:07 +0530

    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-import { bugAdded, bugResolved, getUnresolvedBugs } from "./store/bugs";
+import {
+  bugAdded,
+  bugResolved,
+  bugAssignedToUser,
+  getUnresolvedBugs,
+  getBugsByUser,
+} from "./store/bugs";
 import { projectAdded, projectRemoved } from "./store/projects";
+import { userAdded } from "./store/users";
 import configureStore from "./store/configureStore";
 
 const store = configureStore();
@@ -8,13 +15,17 @@ store.subscribe(() => {
   console.log("Store changed!", store.getState());
 });
 
+store.dispatch(userAdded({ name: "Gitesh" }));
+
 store.dispatch(bugAdded({ description: "Bug 1" }));
 store.dispatch(bugAdded({ description: "Bug 2" }));
 store.dispatch(bugAdded({ description: "Bug 3" }));
-store.dispatch(bugResolved({ id: 1 }));
+store.dispatch(bugAssignedToUser({ bugId: 2, userId: 1 }));
+store.dispatch(bugResolved({ id: 2 }));
 
 store.dispatch(projectAdded({ name: "Project 1" }));
 store.dispatch(projectAdded({ name: "Project 2" }));
 store.dispatch(projectRemoved({ id: 1 }));
 
 console.log(getUnresolvedBugs(store.getState()));
+console.log(getBugsByUser(1)(store.getState()));
