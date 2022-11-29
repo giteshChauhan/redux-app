237db09c3d1116de20e6fb687a4ddd90ed7e54ca
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-11-29 18:30:05 +0530

    updated configureStore.js | added api middleware

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,12 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import logger from "./middleware/logger";
 import toast from "./middleware/toast";
+import api from "./middleware/api";
 import reducer from "./reducer";
 
 export default function () {
   return configureStore({
     reducer,
-    middleware: [...getDefaultMiddleware(), logger, toast],
+    middleware: [...getDefaultMiddleware(), logger, toast, api],
   });
 }
