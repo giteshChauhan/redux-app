82ab1dbef810f8e761c571aad3980b15b31bc99f
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-11-29 18:59:55 +0530

    updated logger.js

--- src/store/middleware/logger.js
+++ src/store/middleware/logger.js
@@ -1,7 +1,5 @@
 const logger = (store) => (next) => (action) => {
-  console.log("store", store);
-  console.log("next", next);
-  console.log("action", action);
+  console.log("Logging! ", action);
   next(action);
 };
 
